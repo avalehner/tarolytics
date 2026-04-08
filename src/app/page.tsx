0c7c0a66b2224d195e68726fa28ee86e2cd42954
Ava Lehner <avalehner@gmail.com>
2026-04-08 15:16:05 -0400

    Finished converting homepage to tyepscript and updating home css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,109 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import { useState } from 'react'
+import './page.css' 
+import CardInput from '@/components/CardInput'
+import DatePicker from '@/components/DatePicker'
+import ReadingSpreadMenu from '@/components/ReadingSpreadMenu'
+import ReadingTopicMenu from '@/components/ReadingTopicMenu'
+import spreadConfig from '@/data/spreadConfig'
+
+const HomePage = () => {
+
+  const [date, setDate] =useState<string>('')
+  const [readingSpread, setReadingSpread] = useState<string>('top-bottom')
+  const [customReadingSpread, setCustomReadingSpread] = useState<string>('')
+  const [readingTopic, setReadingTopic] = useState<string>('card-of-day')
+  const [customReadingTopic, setCustomReadingTopic] = useState<string>('')
+  const [cards, setCards] = useState<string[]>([''])
+  const [notes, setNotes] = useState<string>('')
+
+  const handleAddCard = () => setCards([...cards, '']) //empty card slot to render new card input
+    
+  const handleRemoveCard = (indexToRemove: number) => {
+    const removedCardsArr = cards.filter((_, index) => index !== indexToRemove)
+    setCards(removedCardsArr)
+  }
+
+
+  const renderCardInputs = (readingSpread: string) => {
+    const labels = spreadConfig[readingSpread] || [] //[] to handle situation where spreadConfig is falsy 
+
+    if (readingSpread === 'custom') {
+      return (
+        <>
+        <button className="add-card-btn" onClick={handleAddCard}>ADD CARD</button>
+        {cards.map((_, index) => 
+          <div key={index} className="card-inputs-container">
+          <CardInput
+            cards={cards} 
+            setCards={setCards}
+            label={spreadConfig[readingSpread][0]}
+            index={index}
+          /> 
+          <i className="fa-regular fa-x" onClick={() => handleRemoveCard(index)}></i>
+        </div>
+        )}
+         </>
+      )
+    }
+
+    return (
+      <div className="card-inputs-container">
+        {labels.map((label, index) => 
+          <CardInput
+            cards={cards}
+            setCards={setCards}
+            label={label}
+            index={index}
+            key={index}
+          /> 
+          )
+        }
+      </div>
+    )
+  }
+  
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <>
+      <h1>Tarolytics</h1>
+      <DatePicker 
+        date={date}
+        setDate={setDate}
+      /> 
+      <ReadingTopicMenu 
+        readingTopic={readingTopic}
+        setReadingTopic={setReadingTopic}
+        customReadingTopic={customReadingTopic}
+        setCustomReadingTopic={setCustomReadingTopic}
+      />
+      <ReadingSpreadMenu 
+        setCards={setCards}
+        readingSpread={readingSpread}
+        setReadingSpread={setReadingSpread}
+        customReadingSpread={customReadingSpread}
+        setCustomReadingSpread={setCustomReadingSpread}
+      />
+
+      {renderCardInputs(readingSpread)}
+      <div className="reading-notes">
+        <input
+          type="text"
+          value={notes}
+          placeholder="notes"
+          onChange={(e) => setNotes(e.target.value)}
         />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+      </div>
+      <div>
+        <button className="save-reading-btn">SAVE READING</button>
+        <button className="upload-picture-btn">UPLOAD PICTURE</button>
+      </div>
+    </>
+  )
+
 }
+
+
+export default HomePage
+
