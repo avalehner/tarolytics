28c94c7d3fb525724f45f5b204db3760640b330e
Ava Lehner <avalehner@gmail.com>
2026-04-10 16:35:01 -0400

    installed cors and added cors middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,18 @@
 import 'dotenv/config' //loads environment variables when the server starts 
-import { Express } from "express" //type only 
+import express, { Express } from "express" //type only 
 import readingsRouter from './routes/readings'
-const express = require('express') //runtime value 
+import cors from 'cors'
 
 const app: Express = express() //creates app instance: the request handling logic, doesn't listen for network traffic. knows what to do with requests but doesn't handle any on its own 
 
+//middleware
+app.use(express.json()) //parses incoming JSON request bodies from the front end
+app.use(cors({ origin: 'http://localhost:5173'})) //tells express to only allow requests from the vite dev server
+
+//routes
+app.use('/api/readings', readingsRouter)
+
+//server: actually creates the server and tells it to start listening on port 3000
 app.listen(3000, ()=> {
   console.log('Server running on port 3000')
 }) //actually creates the server and tells it to start listening on port 3000
-
-app.use(express.json()) //this is the middle ware that parses incoming JSON request bodires from the fron end 
-
-app.use('/api/readings', readingsRouter)
