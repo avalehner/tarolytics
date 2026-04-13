28eeaed5119e94f60e50f4584643d6a00ae7eac3
Ava Lehner <avalehner@gmail.com>
2026-04-13 18:01:12 -0400

    update date format

--- client/src/components/ReadingLog.tsx
+++ client/src/components/ReadingLog.tsx
@@ -8,9 +8,16 @@ interface ReadingLogProps {
 }
 
 const ReadingLog = ({reading}: ReadingLogProps) => {
+  
+  const getDate = () => {
+    const date = new Date(reading.reading_date)
+    return date.toLocaleDateString('en-US').replaceAll('/', '.')
+  }
+
+
   return (
     <div className="reading-log">
-      <p>{reading.reading_date}</p>
+      <p>{getDate()}</p>
       <p>{topicLabels[reading.reading_topic] || reading.reading_topic}</p>
       <p>{spreadLabels[reading.spread_type] || reading.spread_type}</p>
       <p>[cards]</p>
