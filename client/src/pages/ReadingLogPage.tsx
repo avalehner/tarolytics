a61e2cf34e2c228fd18adc590366528d864c5ef5
Ava Lehner <avalehner@gmail.com>
2026-04-13 15:56:04 -0400

    added reading logs to readinglog page

--- client/src/pages/ReadingLogPage.tsx
+++ client/src/pages/ReadingLogPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './css/ReadingLogPage.css'
 import SearchMenu from '../components/SearchMenu'
 import DatePicker from '../components/DatePicker'
@@ -6,19 +6,48 @@ import ReadingTopicMenu from '../components/ReadingTopicMenu'
 import ReadingSpreadMenu from '../components/ReadingSpreadMenu'
 import spreadConfig from '../data/spreadConfig'
 import CardInput from '../components/CardInput'
+import { getAllReadings } from '../services/readingService'
+import { ReadingTypes } from '../types'
+import ReadingLog from "../components/ReadingLog"
 
 const ReadingLogPage = () => {
-  const [searchCategory, setSearchCategory] = useState<string>('')
+  const [searchCategory, setSearchCategory] = useState<string>('date')
   const [date, setDate] = useState<string>('')
   const [readingTopic, setReadingTopic] = useState<string>('card-of-day')
   const [customReadingTopic, setCustomReadingTopic] = useState<string>('')
   const [readingSpread, setReadingSpread] = useState<string>('top-bottom')
   const [customReadingSpread, setCustomReadingSpread] = useState<string>('')
   const [cards, setCards] = useState<string[]>([''])
+  const [readings, setReadings] = useState<ReadingTypes[]>([])
+
+  useEffect(()=> {
+    getAllReadings()
+      .then(data => setReadings(data))
+  }, []) //[] tells react to only run the effect once on initial mount
+
+  console.log(readings)
 
   const handleRemoveCard = (indexToRemove: number) => {
     setCards(cards.filter((_, index) => index !== indexToRemove))
     console.log('test')
+  }
+
+  const renderReadingLogs = () => {
+    const readingsList = readings.map((reading, index)=> {
+      if (index < readings.length -1) {
+        return(
+          <>
+            <ReadingLog reading={reading}/>
+            <hr /> 
+          </>
+        )
+      } else {
+        return(
+          <ReadingLog reading={reading}/>
+        )
+      }
+    })
+    return readingsList
   }
 
   return (
@@ -60,6 +89,17 @@ const ReadingLogPage = () => {
             </div>
           </>
         )}  
+      <div className="reading-log-labels">
+        <p>DATE</p>
+        <p>TOPIC</p>
+        <p>SPREAD</p>
+        <p>CARDS</p>
+        <p>NOTES</p>
+        <p>INTERPRETATION</p>
+      </div>
+      <div className="reading-log-container">
+        {renderReadingLogs()}
+      </div>
     </>
   )
 }
