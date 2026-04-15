f32280d7a8eb8906e97964fd710c2625f30703c1
Ava Lehner <avalehner@gmail.com>
2026-04-15 14:23:29 -0400

    created cards fetch requests + types

---
+++ client/src/services/cardsService.ts
@@ -0,0 +1,24 @@
+import { CardTypes, NewCardTypes } from "../types"
+
+export const getCardsForReading = async (readingId: string): Promise<CardTypes[]> => {
+  const response = await fetch(`http://localhost:3000/api/cards/${readingId}`, {
+    method: 'GET', 
+    headers: { 'Content-Type': 'application/json'}, 
+  })
+  if(!response.ok) throw new Error(`Server error: ${response.status}`)
+    
+  const cardData = await response.json()
+  return cardData
+}
+
+export const saveCards = async (data: NewCardTypes): Promise<CardTypes> => {
+  const response = await fetch('http://localhost:3000/api/cards/', {
+    method: 'POST', 
+    headers: { 'Content-Type': 'application/json' }, 
+    body: JSON.stringify(data)
+  })
+  if(!response.ok) throw new Error(`Server error: ${response.status}`)
+  
+  const cardData = await response.json()
+  return cardData
+}
--- client/src/types.ts
+++ client/src/types.ts
@@ -11,3 +11,15 @@ export interface ReadingTypes {
 }
 
 export type NewReadingTypes = Omit<ReadingTypes, 'id' | 'created_at' | 'updated_at' | 'user_id'>
+
+export interface CardTypes {
+  id: string, 
+  reading_id: string, 
+  card_name: string, 
+  position: string, 
+  position_order: number, 
+  created_at: string, 
+  updated_at: string, 
+}
+
+export type NewCardTypes = Omit<CardTypes, 'id' | 'created_at' | 'updated_at'>
