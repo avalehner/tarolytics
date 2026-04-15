7ef2e5cf9d2d543080bcc0c2b9ef4032ca5d61e4
Ava Lehner <avalehner@gmail.com>
2026-04-15 16:01:14 -0400

    added get all cards fetch request

--- client/src/services/cardsService.ts
+++ client/src/services/cardsService.ts
@@ -11,6 +11,17 @@ export const getCardsForReading = async (readingId: string): Promise<CardTypes[]
   return cardData
 }
 
+export const getAllCards = async (): Promise<CardTypes[]> => {
+  const response = await fetch(`http://localhost:3000/api/cards/`, {
+    method: 'GET', 
+    headers: { 'Content-Type': 'application/json'}
+  })
+  if(!response.ok) throw new Error(`Server error: ${response.status}`)
+
+  const cardData = await response.json()
+  return cardData
+}
+
 export const saveCards = async (data: NewCardTypes): Promise<CardTypes> => {
   const response = await fetch('http://localhost:3000/api/cards/', {
     method: 'POST', 
