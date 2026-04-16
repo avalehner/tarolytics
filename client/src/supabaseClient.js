43286cccb35fc990e0f085339f47adc072baa181
Ava Lehner <avalehner@gmail.com>
2026-04-16 13:34:59 -0400

    deleted supabase client

--- client/src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-//get environment variables from .env 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-//creating and exporting supabase client
-export const supabase = createClient(supabaseUrl, supabaseKey)
