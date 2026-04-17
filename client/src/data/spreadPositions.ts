ace3e16d1a4c1d2c49b4b65481729e001f2dfef8
Ava Lehner <avalehner@gmail.com>
2026-04-17 17:36:22 -0400

    rendered card positions

---
+++ client/src/data/spreadPositions.ts
@@ -0,0 +1,54 @@
+const spreadPositions: Record<string, { 
+  cardWidth: number, 
+  positions: {
+    x: number, 
+    y: number, 
+    rotation: number,
+  }[]
+}> = {
+  'single-card': {
+    cardWidth: 55, 
+    positions: [{ x: 50, y: 30, rotation: 0}]
+  }, 
+  'top-bottom': {
+    cardWidth: 48, 
+    positions: [
+      {x: 25, y: 25, rotation: 0 }, 
+      { x: 85, y: 50, rotation: 0 }
+    ]
+  }, 
+  'past-present-future': {
+    cardWidth: 48, 
+    positions: [
+      { x: 5, y: 35, rotation: 0 }, 
+      { x: 57.5, y: 35, rotation: 0 }, 
+      { x: 110, y: 35, rotation: 0 }
+    ]
+  }, 
+  'past-present-future-advice': {
+    cardWidth: 37, 
+    positions: [
+      { x: 0, y: 37, rotation: 0 }, 
+      { x: 40, y: 37, rotation: 0 }, 
+      { x: 80, y: 37, rotation: 0 },
+      { x: 122 , y: 39, rotation: 5 }
+    ]
+  }, 
+  'celtic': {
+    cardWidth: 20, 
+    positions: [
+      { x: 45, y: 68, rotation: 0 }, 
+      { x: 45, y: 68, rotation: 90 }, 
+      { x: 45, y: 14, rotation: 0 }, 
+      { x: 45, y: 123, rotation: 0 },
+      { x: 7, y: 68, rotation: 0 },
+      { x: 83, y: 68, rotation: 0 },
+      { x: 119, y: 135, rotation: 0 },
+      { x: 119, y: 91, rotation: 0 },
+      { x: 119, y: 47, rotation: 0 },
+      { x: 119, y: 3, rotation: 0 },
+    ]
+  }
+}
+
+export default spreadPositions
--- client/src/pages/ViewReadingPage.tsx
+++ client/src/pages/ViewReadingPage.tsx
@@ -2,12 +2,16 @@ import styles from './css/ViewReadingPage.module.css'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { getReadingById } from '../services/readingService'
-import { ReadingTypes } from '../types'
+import { ReadingTypes, CardTypes } from '../types'
 import { format } from "date-fns"
 import { convertDayToWord } from '../util'
+import { getCardsByReadingId } from '../services/cardsService'
+import { getCardImagePath } from '../util'
+import spreadPositions from '../data/spreadPositions'
 
 const ViewReadingPage = () => {
-  const [reading, setReading] = useState<ReadingTypes | null>(null)
+  const [reading, setReading] = useState<ReadingTypes | null>(null) //because this holds a single reading which is just an object, there is no way to represent an empty object so we have to write null
+  const [cards, setCards] = useState<CardTypes[]>([])
 
   const { readingId } = useParams()
 
@@ -15,9 +19,12 @@ const ViewReadingPage = () => {
      if (!readingId) return //makes sure readingId is not null
     getReadingById(readingId)
       .then(data => setReading(data))
+
+    getCardsByReadingId(readingId)
+      .then(data => setCards(data))
   }, [readingId])
 
-  console.log(reading)
+  console.log(cards)
 
   if (!reading) return //makes sure reading is not null
 
@@ -31,40 +38,68 @@ const ViewReadingPage = () => {
     }
   }
 
+  const renderCardImage = (card: CardTypes) => {
+    const {positions, cardWidth} = spreadPositions[reading.spread_type]
+    const {x, y, rotation} = positions[card.position_order]
+    const cardImagePath = getCardImagePath(card.card_name)
+    let cardRotation = rotation
+    if (card.card_name.includes('rx')) cardRotation += 180
+    
+    console.log(cardWidth)
+
+    return (
+      <img 
+      className={styles['card-image']}
+      src={`${cardImagePath}`}
+      style={{ //style is a React prop that accepts a JS object which is y it need 2 brackets 
+        position: 'absolute', 
+        left: `${x}%`, 
+        top: `${y}%`, 
+        transform: `rotate(${cardRotation}deg)`, 
+        width: `${cardWidth}%`
+      }}></img>
+    )
+  }
+
   return (
     <>
-      <div className={styles["reading-info-container"]}>
-        <div className={styles["month-day-container"]}>
-          <h1 className={styles['month']}>{formatDate(reading.reading_date).month}</h1>
-          <h1 className={styles['day']}>{convertDayToWord(formatDate(reading.reading_date).day)}</h1>
-        </div>
-        <h2 className={styles["year"]}>{formatDate(reading.reading_date).year}</h2>
-        <hr className={styles['aesthetic-divider']}/>
-        <div className={styles['details-container']}>
-          <div className={styles["topic-container"]}>
-            <p>topic:</p>
-            <p>{reading.reading_topic}</p>
+      <div className={styles['view-reading-container']}>
+        <div className={styles["reading-info-container"]}>
+          <div className={styles["month-day-container"]}>
+            <h1 className={styles['month']}>{formatDate(reading.reading_date).month}</h1>
+            <h1 className={styles['day']}>{convertDayToWord(formatDate(reading.reading_date).day)}</h1>
           </div>
-          <div className={styles["spread-container"]}>
-            <p>spread:</p>
-            <p>{reading.spread_type}</p>
+          <h2 className={styles["year"]}>{formatDate(reading.reading_date).year}</h2>
+          <hr className={styles['aesthetic-divider']}/>
+          <div className={styles['details-container']}>
+            <div className={styles["topic-container"]}>
+              <p>topic:</p>
+              <p>{reading.reading_topic}</p>
+            </div>
+            <div className={styles["spread-container"]}>
+              <p>spread:</p>
+              <p>{reading.spread_type}</p>
+            </div>
+            <div className={styles["notes-container"]}>
+              <p>notes:</p>
+              <p>{reading.notes}</p>
+            </div>
           </div>
-          <div className={styles["notes-container"]}>
-            <p>notes:</p>
-            <p>{reading.notes}</p>
+          <div className={styles["button-container"]}>
+            <button className={styles["edit-reading-btn"]}>EDIT</button>
+            <button className={styles["clarifyer-btn"]}>CLARIFYER</button>
           </div>
+          <hr className={styles['aesthetic-divider']}/>
+          <div className={styles["interpretation-card"]}>
+            <p className={styles["interpretation"]}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi  ut aliquip ex ea commodo consequat. Duis aute irure dolor in  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla  pariatur.
+            </p>
+          </div>
+          <button className={styles["save-interpretation-btn"]}>SAVE INTERPRETATION</button>
         </div>
-        <div className={styles["button-container"]}>
-          <button className={styles["edit-reading-btn"]}>EDIT</button>
-          <button className={styles["clarifyer-btn"]}>CLARIFYER</button>
+        <div className={styles['spread-display-container']}>
+          {cards.map((card) => renderCardImage(card))}
         </div>
-        <hr className={styles['aesthetic-divider']}/>
-        <div className={styles["interpretation-card"]}>
-          <p className={styles["interpretation"]}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi  ut aliquip ex ea commodo consequat. Duis aute irure dolor in  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla  pariatur.
-          </p>
-        </div>
-        <button className={styles["save-interpretation-btn"]}>SAVE INTERPRETATION</button>
       </div>
     </>
   )
