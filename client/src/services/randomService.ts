9e5538a4d95438f0ef3876691f5b1c6d2b4030a6
Ava Lehner <avalehner@gmail.com>
2026-06-03 16:17:33 -0400

    passed reversal boolean for pulled card logic

--- client/src/services/randomService.ts
+++ client/src/services/randomService.ts
@@ -1,17 +1,20 @@
 export const getRandomSequence = async (
   numCards: number,
+  isReversals: boolean,
 ): Promise<number[]> => {
-  const randomResponse = await fetch(`http://localhost:3000/api/random`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
+  const randomResponse = await fetch(
+    `http://localhost:3000/api/random/${isReversals}`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    },
+  );
 
   if (!randomResponse.ok) {
     throw new Error(
       `Server error [getRandomSequence - randomService.ts]: ${randomResponse.status}`,
     );
   }
-
   const randomData = await randomResponse.json();
   console.log("randomData:", randomData);
   const randomCardIndeces = randomData.slice(0, numCards);
--- server/src/routes/random.ts
+++ server/src/routes/random.ts
@@ -3,11 +3,16 @@ import { Router, Request, Response } from "express";
 //create router instance
 const randomRouter = Router();
 
-randomRouter.get("/", async (req: Request, res: Response) => {
+randomRouter.get("/:isReversals", async (req: Request, res: Response) => {
   try {
+    const { isReversals } = req.params;
+    const isReversalsBoolean = req.params.isReversals === "true";
+    let maxNumber = 78;
+    if (isReversalsBoolean) maxNumber = 156;
+    console.log("maxNumber", maxNumber);
     //format=plain means plain text
     const randomResponse = await fetch(
-      `https://www.random.org/sequences/?min=1&max=156&col=1&format=plain&rnd=new`,
+      `https://www.random.org/sequences/?min=1&max=${maxNumber}&col=1&format=plain&rnd=new`,
       {
         method: "GET",
         headers: { "Content-Type": "application/json" },
@@ -22,7 +27,7 @@ randomRouter.get("/", async (req: Request, res: Response) => {
 
     const randomText = await randomResponse.text();
 
-    console.log("random-text", randomText);
+    // console.log("random-text", randomText);
 
     //.trim() removes any leading/trailing whitespace
     //numbers are returned with '\n' as a splitter
