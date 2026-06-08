8c08b4830a43483830feb1729b2d6c0f562c3201
Ava Lehner <avalehner@gmail.com>
2026-06-08 12:46:07 -0400

    updated to datepicker types

--- client/src/components/DatePicker.tsx
+++ client/src/components/DatePicker.tsx
@@ -3,19 +3,22 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./css/DatePicker.css";
 
 interface DatePickerProps {
-  date: string;
-  setDate: (value: string) => void;
+  date: Date | null;
+  setDate: (value: Date | null) => void;
 }
 
 const DatePicker = ({ date, setDate }: DatePickerProps) => {
   return (
     <ReactDatePicker
-      selected={date ? new Date(date + "T00:00:00") : null} //converts date from string into date object os react date picker can understand it
-      onChange={
-        (selectedDate: Date | null) => {
-          if (selectedDate) setDate(selectedDate.toLocaleDateString("en-CA"));
-        } //converts selecteddate back into a string for the setDate function, react-date-picker only uses react Date type
-      }
+      showYearDropdown
+      showMonthDropdown
+      dropdownMode="select"
+      yearDropdownItemNumber={100}
+      scrollableYearDropdown
+      selected={date ? new Date(date) : null}
+      onChange={(selectedDate: Date | null) => {
+        if (selectedDate) setDate(selectedDate);
+      }}
       placeholderText="mm/dd/yyyy"
       dateFormat="MM/dd/yyyy"
     />
