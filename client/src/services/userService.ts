5d1bfbd1d0f4d314bed6dd44b533330421fb2e20
Ava Lehner <avalehner@gmail.com>
2026-06-08 12:16:09 -0400

    userservice created

---
+++ client/src/services/userService.ts
@@ -0,0 +1,20 @@
+import type { UserProfileTypes, UserTypes } from "../types";
+
+export const saveProfileData = async (
+  userId: string,
+  userProfile: UserProfileTypes,
+): Promise<UserTypes> => {
+  const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userProfile),
+  });
+
+  if (!response.ok)
+    throw new Error(
+      `Server error [saveProfileData - userService.ts]: ${response.status}`,
+    );
+
+  const updatedUserData = await response.json();
+  return updatedUserData;
+};
