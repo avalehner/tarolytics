c5b87bb593d8f31b1bb751f310d65250f417f609
Ava Lehner <avalehner@gmail.com>
2026-06-08 12:15:15 -0400

    users router

---
+++ server/src/routes/users.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from "express";
+import pool from "../db";
+
+const usersRouter = Router();
+
+usersRouter.patch("/:userId", async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const { birthday, birth_time, birth_timezone, birth_location } = req.body;
+
+    const dbResponse = await pool.query(
+      `
+      UPDATE users 
+      SET birthday = $1, birth_time = $2, birth_timezone = $3, birth_location = $4
+      WHERE id =  $5`,
+      [birthday, birth_time, birth_timezone, birth_location, userId],
+    );
+
+    const updatedUser = await dbResponse.rows[0];
+    res.status(201).json(updatedUser);
+  } catch (error) {
+    const message = error instanceof Error ? error.message : "Unknown errors";
+    res.status(500).json({ error: message });
+  }
+});
+
+export default usersRouter;
