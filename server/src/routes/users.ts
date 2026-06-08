f80e77b59a0c79f44b9717e870ccb4ebd5fe8a3d
Ava Lehner <avalehner@gmail.com>
2026-06-08 16:54:07 -0400

    updates to user patch route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -7,16 +7,21 @@ usersRouter.patch("/:userId", async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
     const { birthday, birth_time, birth_timezone, birth_location } = req.body;
+    const pointStr = birth_location
+      ? `(${birth_location.lng}, ${birth_location.lat})`
+      : null;
+    const birthCity = birth_location?.formatted;
 
     const dbResponse = await pool.query(
       `
       UPDATE users 
-      SET birthday = $1, birth_time = $2, birth_timezone = $3, birth_location = $4
-      WHERE id =  $5`,
-      [birthday, birth_time, birth_timezone, birth_location, userId],
+      SET birthday = $1, birth_time = $2, birth_timezone = $3, birth_location = $4, birth_city = $5
+      WHERE id =  $6
+      RETURNING *;`,
+      [birthday, birth_time, birth_timezone, pointStr, birthCity, userId],
     );
 
-    const updatedUser = await dbResponse.rows[0];
+    const updatedUser = dbResponse.rows[0];
     res.status(201).json(updatedUser);
   } catch (error) {
     const message = error instanceof Error ? error.message : "Unknown errors";
